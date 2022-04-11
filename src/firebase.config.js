4ceaa9f4b171b58b4ffa2a0956618972ae6bf2c4
Alexjandro Mendoza <alexjandromendoza@macbook-pro.lan>
2022-04-11 12:55:39 -0400

    project start

---
+++ src/firebase.config.js
@@ -0,0 +1,17 @@
+
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore'
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAB6U-SGO-0ktGgUu3CIHpk5KhCzXmfdQM",
+  authDomain: "house-marketplace-app-c44d1.firebaseapp.com",
+  projectId: "house-marketplace-app-c44d1",
+  storageBucket: "house-marketplace-app-c44d1.appspot.com",
+  messagingSenderId: "549678831404",
+  appId: "1:549678831404:web:fa7da5425f395d3e6d3e89"
+};
+
+// Initialize Firebase
+export const db = getFirestore()
