1497d88ae489f45bd41e879021e99531ad580ece
Alexjandro Mendoza <alexjandromendoza@macbook-pro.mynetworksettings.com>
2022-04-12 18:28:35 -0400

    router working, adding navbar now

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import {useNavigate, useLocation} from 'react-router-dom'
+import {ReactComponent as OfferIcon} from '../assets/svg/localOfferIcon.svg'
+import {ReactComponent as exploreIcon} from '../assets/svg/localexploreIcon.svg'
+import {ReactComponent as PersonOutlineIcon} from '../assets/svg/localPersonOutlineIcon.svg'
+
+
+
+function Navbar() {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
