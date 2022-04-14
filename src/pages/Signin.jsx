00661c2c0954cc64e016253b16d69c89f3232e2f
Alexjandro Mendoza <alexjandromendoza@macbook-pro.mynetworksettings.com>
2022-04-14 15:04:52 -0400

    User authentication update, user name is shown once logged in, adding react toastify now

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,5 +1,6 @@
 import {useState} from 'react'
 import {Link, useNavigate} from 'react-router-dom'
+import {getAuth, signInWithEmailAndPassword} from 'firebase/auth'
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
@@ -22,6 +23,29 @@ function SignIn() {
 
   }
 
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+
+      const auth = getAuth()
+
+      const userCredential = await signInWithEmailAndPassword(auth, email, password)
+  
+      if (userCredential.user) {
+        navigate('/')
+      }
+
+    }
+
+    catch (error) {
+      console.log(error);
+
+    }
+
+
+  }
+
 
 
   return (
@@ -30,8 +54,8 @@ function SignIn() {
       <header>
       <p className = 'pageHeader'>Welcome Back ! </p>
       </header>
-      <main>
-        <form>
+      
+        <form onSubmit={onSubmit}>
           <input type='email' 
           className="emailInput" 
           placeholder='Email' 
@@ -72,7 +96,7 @@ function SignIn() {
         <Link to='/sign-up' className='registerLink'>
           Sign Up Instead
         </Link>
-      </main>
+      
       
     </div>
 
