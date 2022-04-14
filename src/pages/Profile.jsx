e37eb0fbc4b7f6217230be61a075e5b300bb7cde
Alexjandro Mendoza <alexjandromendoza@macbook-pro.mynetworksettings.com>
2022-04-14 15:24:03 -0400

    logout button added

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,18 +1,35 @@
 import {getAuth} from 'firebase/auth'
 import {useEffect, useState} from 'react'
+import {useNavigate, Link} from 'react-router-dom'
 
 function Profile() {
-
-  const [user, setUser] = useState (null)
-
   const auth = getAuth()
-  useEffect(() => {
-    setUser(auth.currentUser)
-  }, [])
+  const [formData, setFormData] = useState ({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email
+  })
+
+  const {name, email} = formData
+
+  
+  const navigate = useNavigate()
+
+  const onLogout = () => {
+    auth.signOut()
+    navigate('/')
+  }
 
 
+  return <div className = 'profile' >
+    <header className="profileHeader">
+      <p className="pageHeader">My Profile</p>
+        <button type='button' className="logOut" onClick={onLogout}>
+          Logout
+        </button>
+      
+    </header>
 
-  return user ? <h1>{user.displayName}</h1>  : 'Not Logged In'
+  </div>
   
 }
 
