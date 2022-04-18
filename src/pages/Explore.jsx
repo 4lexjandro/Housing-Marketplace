bb4c7c36643dc254acc208a4fe9f7b8af3282b6e
Alexjandro Mendoza <alexjandromendoza@macbook-pro.mynetworksettings.com>
2022-04-18 18:14:13 -0400

    listing item operating fine, bathrooms and bedrooms font not identical however

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -2,6 +2,7 @@ import {Link} from 'react-router-dom'
 import rentCategoryImage from '../assets/jpg/rentCategoryImage.jpg'
 import sellCategoryImage from '../assets/jpg/sellCategoryImage.jpg'
 
+
 function Explore() {
   return (
 
@@ -11,7 +12,7 @@ function Explore() {
         <p className="pageHeader">Explore</p>
       </header>
       <main>
-        {/* Slideer */}
+        {/* Slider */}
         <p className="exploreCategoryHeading">Categories</p>
         <div className='exploreCategories'>
           <Link to='/category/rent' >
@@ -21,7 +22,7 @@ function Explore() {
           <p className="exploreCategoryName">Places for rent</p>
           </Link>
 
-          <Link to='/category/sell' >
+          <Link to='/category/sale' >
           <img src={sellCategoryImage} 
           alt="sell"
           className='exploreCategoryImg' />
